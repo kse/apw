890166503531d1148c1e0d2c285fc8288f2bde74
Sándor Czettner <sandor@czettner.hu>
2014-12-13 14:02:25 +0000

    Force coding standards

--- widget.lua
+++ widget.lua
@@ -34,7 +34,7 @@ local color_mute    = '#be2a15' -- foreground color when muted
 local color_bg_mute = color_bg --'#532a15' -- background color when muted
 local mixer         = 'pavucontrol' -- mixer command
 local mixer_class   = 'Pavucontrol'
-local veromix	    = 'veromix' --veromix command
+local veromix       = 'veromix' --veromix command
 local veromix_class = 'veromix'
 
 -- default configuration overridden by Beautiful theme
@@ -69,122 +69,122 @@ pulseLayout:add(pulseBox)
 local pulseWidget = wibox.widget.background(wibox.layout.margin(pulseLayout, margin_left, margin_right, margin_top, margin_bottom), color_bg)
 
 function pulseWidget.setColor(mute)
-	if mute then
-		pulseBar:set_color(color_mute)
-		pulseBar:set_background_color(color_bg_mute)
-	else
-		pulseBar:set_color(color)
-		pulseBar:set_background_color(color_bg)
-	end
+    if mute then
+        pulseBar:set_color(color_mute)
+        pulseBar:set_background_color(color_bg_mute)
+    else
+        pulseBar:set_color(color)
+        pulseBar:set_background_color(color_bg)
+    end
 end
 
 local function _update()
-	pulseBar:set_value(p.Volume)
-	text = p.Perc 
-	pulseBox:set_text(''..text..'')
-	pulseWidget.setColor(p.Mute)
+    pulseBar:set_value(p.Volume)
+    text = p.Perc 
+    pulseBox:set_text(''..text..'')
+    pulseWidget.setColor(p.Mute)
 end
 
 function pulseWidget.SetMixer(command)
-	mixer = command
+    mixer = command
 end
 
 function pulseWidget.Up()
-	p:SetVolume(p.Volume + pulseBar.step)
-	notid = naughty.notify({ text = 'Volume: ' .. p.Perc, replaces_id = notid }).id
-	_update()
-end	
+    p:SetVolume(p.Volume + pulseBar.step)
+    notid = naughty.notify({ text = 'Volume: ' .. p.Perc, replaces_id = notid }).id
+    _update()
+end 
 
 function pulseWidget.Down()
-	p:SetVolume(p.Volume - pulseBar.step)
-	notid = naughty.notify({ text = 'Volume: ' .. p.Perc, replaces_id = notid }).id
-	_update()
-end	
+    p:SetVolume(p.Volume - pulseBar.step)
+    notid = naughty.notify({ text = 'Volume: ' .. p.Perc, replaces_id = notid }).id
+    _update()
+end 
 
 function pulseWidget.ToggleMute()
-	p:ToggleMute()
-	_update()
+    p:ToggleMute()
+    _update()
 end
 
 function pulseWidget.Update()
-	p:UpdateState()
-	 _update()
+    p:UpdateState()
+     _update()
 end
 
 function pulseWidget.LaunchMixer()
-	run_or_kill(mixer,  { class = mixer_class })
-	_update()
+    run_or_kill(mixer,  { class = mixer_class })
+    _update()
 end
 
 function pulseWidget.LaunchVeromix()
-	run_or_kill(veromix, { class = veromix_class })
-	_update()	
+    run_or_kill(veromix, { class = veromix_class })
+    _update()   
 end
 
 
 
 function run_or_kill(cmd, properties)
-   local clients = client.get()
-   local focused = awful.client.next(0)
-   local findex = 0
-   local matched_clients = {}
-   local n = 0
-   for i, c in pairs(clients) do
-      --make an array of matched clients
-      if match(properties, c) then
-         n = n + 1
-         matched_clients[n] = c
-         if c == focused then
-            findex = n
-         end
-      end
-   end
-   if n > 0 then
-      local c = matched_clients[1]
-      -- if the focused window matched switch focus to next in list
-      if 0 < findex and findex < n then
-         c = matched_clients[findex+1]
-      end
-      local ctags = c:tags()
-      if #ctags == 0 then
-         -- ctags is empty, show client on current tag
-         local curtag = awful.tag.selected()
-         awful.client.movetotag(curtag, c)
-      else
-         -- Otherwise, pop to first tag client is visible on
-         awful.tag.viewonly(ctags[1])
-      end
-      -- And then kill the client
-      c:kill()
-      return
-   end
-   awful.util.spawn(cmd)
+    local clients = client.get()
+    local focused = awful.client.next(0)
+    local findex = 0
+    local matched_clients = {}
+    local n = 0
+    for i, c in pairs(clients) do
+        --make an array of matched clients
+        if match(properties, c) then
+            n = n + 1
+            matched_clients[n] = c
+            if c == focused then
+                findex = n
+            end
+        end
+    end
+    if n > 0 then
+        local c = matched_clients[1]
+        -- if the focused window matched switch focus to next in list
+        if 0 < findex and findex < n then
+            c = matched_clients[findex+1]
+        end
+        local ctags = c:tags()
+        if #ctags == 0 then
+            -- ctags is empty, show client on current tag
+            local curtag = awful.tag.selected()
+            awful.client.movetotag(curtag, c)
+        else
+            -- Otherwise, pop to first tag client is visible on
+            awful.tag.viewonly(ctags[1])
+        end
+        -- And then kill the client
+        c:kill()
+        return
+    end
+    awful.util.spawn(cmd)
 end
 
 -- Returns true if all pairs in table1 are present in table2
 function match (table1, table2)
-   for k, v in pairs(table1) do
-      if table2[k] ~= v and not table2[k]:find(v) then
-         return false
-      end
-   end
-   return true
+    for k, v in pairs(table1) do
+        if table2[k] ~= v and not table2[k]:find(v) then
+            return false
+        end
+    end
+    return true
 end
 
 function pulseWidget.getTextBox()
-	return pulseBox
+    return pulseBox
 end
 
 
 -- register mouse button actions
 buttonsTable = awful.util.table.join(
-		awful.button({ }, 1, pulseWidget.LaunchVeromix),
-		awful.button({ }, 12, pulseWidget.ToggleMute),
-		awful.button({ }, 2, pulseWidget.ToggleMute),
-		awful.button({ }, 3, pulseWidget.LaunchMixer),
-		awful.button({ }, 4, pulseWidget.Up),
-		awful.button({ }, 5, pulseWidget.Down)
-	)
+        awful.button({ }, 1, pulseWidget.LaunchVeromix),
+        awful.button({ }, 12, pulseWidget.ToggleMute),
+        awful.button({ }, 2, pulseWidget.ToggleMute),
+        awful.button({ }, 3, pulseWidget.LaunchMixer),
+        awful.button({ }, 4, pulseWidget.Up),
+        awful.button({ }, 5, pulseWidget.Down)
+    )
 pulseWidget:buttons(buttonsTable)
 pulseBox:buttons(buttonsTable)
 
